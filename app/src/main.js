564532417431a093267b3d533e9e55e8ba659b19
chalaschek <chalaschek@gmail.com>
2014-10-15 21:20:12 -0400

    Cleaned up shell

--- app/src/main.js
+++ app/src/main.js
@@ -3,18 +3,8 @@ define(function(require, exports, module) {
   'use strict';
   // import dependencies
   var Engine = require('famous/core/Engine');
-  var Surface = require('famous/core/Surface');
 
   var mainContext = Engine.createContext();
 
   mainContext.setPerspective(1000);
-
-  var square = new Surface({
-    size: [100, 100],
-    properties: {
-      backgroundColor: '#cccccc'
-    }
-  });
-
-  mainContext.add( square );
 });
