95a2ff7971afea18b3412785385385a50317e652
chalaschek <chalaschek@gmail.com>
2014-10-15 23:57:12 -0400

    Cube example added

---
+++ app/src/cube.js
@@ -0,0 +1,70 @@
+/* globals define */
+define(function(require, exports, module) {
+  'use strict';
+
+  // import dependencies
+  var Engine = require('famous/core/Engine');
+  var Surface = require('famous/core/Surface');
+  var Transform = require('famous/core/Transform');
+  var Modifier = require('famous/core/Modifier');
+  var View = require('famous/core/View');
+
+  function Cube(options){
+    View.call(this, options);
+
+    this.size = options.size || 200;
+
+    this.initSurfaces();
+  }
+
+  Cube.prototype = Object.create(View.prototype);
+  Cube.prototype.constructor = Cube;
+
+  Cube.prototype.initSurfaces = function(){
+
+    var surfaceRotations = [
+      [0, 0, 0],
+      [0, degreesToRadian(90), 0],
+      [0, degreesToRadian(-90), 0],
+      [0, degreesToRadian(180), 0],
+      [degreesToRadian(90), 0, 0],
+      [degreesToRadian(-90), 0, 0]
+    ];
+
+    var surfaceTranslations = [
+      [0, 0, this.size/2],
+      [this.size/2, 0, 0],
+      [-this.size/2, 0, 0],
+      [0, 0, -this.size/2],
+      [0, this.size/2, 0],
+      [0, -this.size/2, 0]
+    ];
+
+    for(var i = 0; i < 6; i++){
+      var translationModifier = new Modifier({
+        transform: Transform.translate.apply(this, surfaceTranslations[i] )
+      });
+
+      var rotationModifier = new Modifier({
+        transform: Transform.rotate.apply(this, surfaceRotations[i])
+      });
+
+      var surface = new Surface({
+        size: [this.size, this.size],
+        properties: {
+          backgroundColor: '#cccccc',
+          border: '1px solid #333'
+        },
+        classes: ['backfaceVisibility']
+      });
+
+      this.add(translationModifier).add(rotationModifier).add(surface);
+    }
+  }
+
+  function degreesToRadian(deg){
+    return deg * Math.PI/180;
+  }
+
+  module.exports = Cube;
+});
--- app/src/main.js
+++ app/src/main.js
@@ -4,40 +4,28 @@ define(function(require, exports, module) {
 
   // import dependencies
   var Engine = require('famous/core/Engine');
-  var Surface = require('famous/core/Surface');
   var Transform = require('famous/core/Transform');
-  var StateModifier = require('famous/modifiers/StateModifier');
-  var SpringTransition = require('famous/transitions/SpringTransition');
+  var Modifier = require('famous/core/Modifier');
+  var Cube = require('./cube');
 
   var mainContext = Engine.createContext();
 
   mainContext.setPerspective(1000);
 
-  var stateModifier = new StateModifier({
+  var angle = 1;
+  var rotateModifier = new Modifier({
+    origin: [0.5, 0.5],
+    align: [0.5, 0.5],
+    transform: function(){
+      angle += 0.02;
+      return Transform.rotate(angle, angle, 0);
+    }
+  });
+
+  var cube = new Cube({
     origin: [0.5, 0.5],
     align: [0.5, 0.5]
   });
 
-  var square = new Surface({
-    size: [200, 200],
-    properties: {
-      backgroundColor: '#cccccc'
-    },
-    classes: ['backfaceVisibility']
-  });
-
-  stateModifier.setTransform(Transform.rotateY(1000), {
-    method: SpringTransition,
-    period: 500,
-    dampingRatio: 0.2
-  });
-
-  stateModifier.setTransform(Transform.rotateY(0), {
-    method: SpringTransition,
-    period: 500,
-    dampingRatio: 0.2
-  });
-
-
-  mainContext.add(stateModifier).add(square);
+  mainContext.add(rotateModifier).add(cube);
 });
