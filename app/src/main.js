522d190f885e0a5ce0e86b8bd9b13753ff9cb916
chalaschek <chalaschek@gmail.com>
2014-10-15 21:35:46 -0400

    Added rotating example

--- app/src/main.js
+++ app/src/main.js
@@ -6,21 +6,28 @@ define(function(require, exports, module) {
   var Engine = require('famous/core/Engine');
   var Surface = require('famous/core/Surface');
   var Modifier = require('famous/core/Modifier');
+  var Transform = require('famous/core/Transform');
 
   var mainContext = Engine.createContext();
 
   mainContext.setPerspective(1000);
 
+  var initialTime = Date.now();
+
   var centerModifier = new Modifier({
     origin: [0.5, 0.5],
-    align: [0.5, 0.5]
+    align: [0.5, 0.5],
+    transform: function(){
+      return Transform.rotateY(0.002 * (Date.now() - initialTime));
+    }
   });
 
   var square = new Surface({
-    size: [100, 100],
+    size: [200, 200],
     properties: {
       backgroundColor: '#cccccc'
-    }
+    },
+    classes: ['backfaceVisibility']
   });
 
   mainContext.add(centerModifier).add( square );
